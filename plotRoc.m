060690133da79f9fa6fd98ccea792a56ccedc8cf
Erik Reinertsen <erikrtn@gmail.com>
2017-08-06 20:17:01 -0400

    revised plotRoc input args now a structure so we can plot multiple experiments overlaid on same AUROC plot

---
+++ plotRoc.m
@@ -0,0 +1,51 @@
+function plotRoc(rocData, legendStr)
+
+% Initialize figure
+figure('Position', [100 100 800 600]);
+hold on;
+
+% Create cell array for linestyle
+linestyles = generateLinestyles(8);
+
+for i = 1:numel(rocData)
+    plot(rocData(i).xTest, rocData(i).yTest, ...
+         linestyles{i}, 'linewidth', 2);
+ 
+% plot(optrocptTrain(1), optrocptTrain(2), 'o', 'MarkerEdgeColor', ...
+%     inputColors{1}, 'LineWidth', 4, 'MarkerSize', 13);
+% plot(optrocptTest(1), optrocptTest(2), 'o', 'MarkerEdgeColor', ...
+%     inputColors{2}, 'LineWidth', 4, 'MarkerSize', 13);
+end
+ 
+% Remove border around figure
+box off;
+
+ax = gca;
+
+% Set up labels, font size, etc.
+ax.XLabel.String = 'False positive rate';
+ax.YLabel.String = 'True positive rate';
+
+ylim([0 1]);
+ytickVals = [0:0.2:1];
+set(gca,'YTick',ytickVals)
+set(gca,'YTickLabel', cellstr(num2str(ytickVals(:), '%1.2f')) );
+
+xlim([0 1]);
+x = [0:0.2:1];
+set(gca,'XTick',x)
+set(gca,'XTickLabel', cellstr(num2str(x(:), '%1.2f')) );
+
+
+legend(legendStr, 'location', 'best');
+legend boxoff;
+
+set(gca, 'fontsize', 18);
+
+% Set background to white
+set(gcf, 'color', 'w');
+
+% Expand axes to fill figure
+tightfig;
+
+end % end function
