9ab74bbb2c77f922285d1637715a833af9661f16
Erik Reinertsen <erikrtn@gmail.com>
2017-08-06 20:16:35 -0400

    linestyles now has more shapes and up to 8 unique elements before repeating

--- generateLinestyles.m
+++ generateLinestyles.m
@@ -3,23 +3,28 @@ function linestyles = generateLinestyles(numLines)
 % Generates cell array of linestyles
 
 if nargin < 1
-    numLines = 4;
+    numLines = 8;
 end
 
-linestyles{1} = '-';
-linestyles{2} = '--';
-linestyles{3} = ':';
-linestyles{4} = '-.';
+linestyles{1} = '-.v';
+linestyles{2} = '-.p';
+linestyles{3} = '-.d';
+linestyles{4} = '-.<';
+linestyles{5} = '-.*';
+linestyles{6} = '-.>';
+linestyles{7} = '-.h';
+linestyles{8} = '-.s';
+
 linestyles = linestyles(:); % convert to col vector
 
-if numLines > 4
+if numLines > 8
     % Replicate the vector
-    repeats = floor(numLines / 4);
+    repeats = floor(numLines / 8);
     linestyles = repmat(linestyles, repeats, 1);
     
     % Loop over and repeat until we finish off the rest
     idx_end_of_repeats = length(linestyles);
-    remainder = mod(numLines, 4);
+    remainder = mod(numLines, 8);
     for extras = 1:remainder
         linestyles{idx_end_of_repeats+extras} = linestyles{extras};
     end
