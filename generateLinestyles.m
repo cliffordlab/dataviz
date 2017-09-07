03e43cea0ad2aaa702b0f05686ee0c32336a8c1b
Erik Reinertsen <erikrtn@gmail.com>
2017-09-07 01:07:56 -0700

    added a 9th line style and fixed loop over code to repeat linestypes if user specifies >9 for input arg (# of linestyles)

--- generateLinestyles.m
+++ generateLinestyles.m
@@ -3,7 +3,7 @@ function linestyles = generateLinestyles(numLines)
 % Generates cell array of linestyles
 
 if nargin < 1
-    numLines = 8;
+    numLines = 9;
 end
 
 linestyles{1} = '-.v';
@@ -14,17 +14,18 @@ linestyles{5} = '-.*';
 linestyles{6} = '-.>';
 linestyles{7} = '-.h';
 linestyles{8} = '-.s';
+linestyles{9} = '-.o';
 
 linestyles = linestyles(:); % convert to col vector
 
-if numLines > 8
+if numLines > 9
     % Replicate the vector
-    repeats = floor(numLines / 8);
+    repeats = floor(numLines / numLines);
     linestyles = repmat(linestyles, repeats, 1);
     
     % Loop over and repeat until we finish off the rest
     idx_end_of_repeats = length(linestyles);
-    remainder = mod(numLines, 8);
+    remainder = mod(numLines, numLines);
     for extras = 1:remainder
         linestyles{idx_end_of_repeats+extras} = linestyles{extras};
     end
